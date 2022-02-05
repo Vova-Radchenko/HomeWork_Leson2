8ef182a4d932f1a930a5910c8899df3843864ca9
Vova-Radchenko <vettel070897@gmail.com>
2022-02-05 10:21:07 +0200

    commit

--- HomeWork_Leson2/Program.cs
+++ HomeWork_Leson2/Program.cs
@@ -6,15 +6,7 @@ namespace HomeWork_Leson2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // test git
-<<<<<<< HEAD
-            // something for master branch
-            Console.WriteLine("Hello master branch");
-=======
-            // for test new branch
-            Console.WriteLine("Hello test branch");
->>>>>>> test-branch1
+            
         }
     }
 }
